f450c385351bd482d4985e0ab279cd0353a7a2bc
gustavonunez <guscostaf@gmail.com>
2021-07-08 11:28:29 -0300

    APA settings

--- core/publications/templates.php
+++ core/publications/templates.php
@@ -468,12 +468,12 @@ class TP_HTML_Publication_Template
 
         // if with bibtex
         if ($settings['show_bibtex'] === true) {
-            $bibtex = self::get_info_button(__('BibTeX', 'teachpress'), __('Mostrar entrada APA', 'teachpress'), 'bibtex', $container_id) . $separator;
+            $bibtex = self::get_info_button(__('BibTeX', 'teachpress'), __('Show BibTeX entry', 'teachpress'), 'bibtex', $container_id) . $separator;
             $is_button = true;
         }
 
         if ($settings['show_bibtex'] === true) {
-            $apa = self::get_info_button(__('APA', 'teachpress'), __('Show APA entry', 'teachpress'), 'apa', $container_id) . $separator;
+            $apa = self::get_info_button(__('APA', 'teachpress'), __('Mostrar entrada APA', 'teachpress'), 'apa', $container_id) . $separator;
             $is_button = true;
         }
 
